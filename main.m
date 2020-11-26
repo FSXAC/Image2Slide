974f38e69f92d74f47c424025c6067996e1b41b1
Muchen He <i@muchen.ca>
2020-11-25 18:39:13 -0800

    added color correction

--- main.m
+++ main.m
@@ -10,7 +10,7 @@ HOUGH_RHO_RES = 1;
 HOUGH_THETA_RES = 0.2;
 
 
-in_file = 'classroom.png';
+in_file = 'classroom.jpg';
 out_file = 'classroom_scan.png';
 
 img = imread(in_file);
@@ -18,12 +18,6 @@ img = imread(in_file);
 % Convert to greyscale
 img_bw = uint8(rgb2gray(img));
 [img_bin, thres] = binaryImage2(img_bw);
-
-if (SHOW_DEBUG)
-    figure;
-    montage({img_bw, img_bin},'Size',[1 2]);
-end
-
 
 %% Apply edge detection filter (note: sobel is not suitable because lines could be diagonal)
 img_grad = edge_grad(img_bin, thres);
@@ -35,7 +29,7 @@ img_grad = edge_grad(img_bin, thres);
 H = H .^ 2;
 
 %% Find points from H (binary search) (0.7 seconds vs 1.75s of old method)
-peaks = hough_search(H, 4, 10);
+peaks = hough_search(H, 4, 50);
 lines = houghlines(img_bw, T, R, peaks);
 
 line_verts = hough_lines2verts(lines);
@@ -49,25 +43,56 @@ draw_detection(img, line_verts, points);
 img_cropped = crop2doc(img_rectified, transformation, ref, points);
 
 %% Document segmentation (TODO: use the rice example from HW4)
-img_doc = imadjust(rgb2gray(img_cropped));
+img_cropped_grey = rgb2gray(img_cropped);
+img_doc = imadjust(img_cropped_grey);
 img_doc = imbinarize(img_doc, 'adaptive', 'Sensitivity', 0.630000, 'ForegroundPolarity', 'bright');
 figure;
 imshow(img_doc);
 
 %% Save the image
-imwrite(img_doc, out_file);
+% imwrite(img_doc, out_file);
+
+%% Perform colour correction
+% Masking the background of the document
+bg_mask = imadjust(img_cropped_grey) > 220;
+img_bg_seg = repmat(uint8(bg_mask), 1, 1, 3) .* img_cropped;
+
+% 3 Channels of histogram don't line up (so let's apply transformations
+% independently to each channel so they look white
+figure;
+imshow(img_bg_seg);
+% [R, G, B] = rgbhist(img_bg_seg);
+img_cropped_c = bumpEqualize(double(img_cropped), img_bg_seg, 240);
+% rgbhist(img_cropped_c);
+figure;
+imshow(uint8(img_cropped_c));
+
+% Apply contrast
+img_cropped_c = img_cropped_c .* 2.0 - 255;
+figure;
+imshow(uint8(img_cropped_c));
+
+% Apply segment boost
+img_boosted = img_cropped_c + (img_doc - 0.2) * 60;
+figure;
+imshow(uint8(img_boosted));
+
+% x = 0:255;
+% x1 = 0.5 * 255 * (sin(x * pi / 255 - pi / 2) + 1);
+% x2 = min(x .^ 2 / (255 * 0.5), 255);
+% x3_shift = 50;
+% x3 = 255 * (sin((x - x3_shift) * pi / 255 - pi / 4).^2);
+% x3(1:64+x3_shift) = 0;
+% x3(191+x3_shift:end) = 255;
+% % plot(x3);
+% % 
+% i1 = intlut(uint8(img_cropped_c), uint8(x1));
+% i2 = intlut(img_cropped, uint8(x2));
+% i3 = intlut(img_cropped, uint8(x3));
+% figure;
+% imshow(i3);
 
 %% Functions
-
-function img = edge_sobel(I, thres)
-    gx = [-1 -2 -1; 0 0 0; 1 2 1];
-    gy = [-1 0 1; -2 0 2; -1 0 1];
-    img_gx = conv2(I, gx);
-    img_gy = conv2(I, gy);
-    img = abs(img_gx) + abs(img_gy);
-    img(img < thres) = 0;
-end
-
 function img = edge_grad(I, thres)
     img = imgradient(I);
     img(img < thres) = 0;
@@ -90,14 +115,18 @@ function points = hough_search(H, n, max_epoch)
         points = [r_idx, t_idx];
 
         if size(points, 1) > n
-            ratio_hi = ratio;
-        elseif size(points, 1) < n
             ratio_lo = ratio;
+        elseif size(points, 1) < n
+            ratio_hi = ratio;
         else
             break
         end
         
         epoch = epoch + 1;
+    end
+    
+    if epoch >= max_epoch
+        error('Max epoch too low');
     end
 end
 
@@ -203,4 +232,45 @@ function img_cropped = crop2doc(img, transformation, ref, points)
     maxnp = max(new_points);
 
     img_cropped = imcrop(img, [minnp, maxnp - minnp]);
+end
+
+% get RGB histogram
+function [r, g, b] = rgbhist(img)
+    r = imhist(img(:, :, 1));
+    g = imhist(img(:, :, 2));
+    b = imhist(img(:, :, 3));
+    figure;
+    plot(r, 'r');
+    hold on
+    plot(g, 'g');
+    plot(b, 'b');
+    hold off
+end
+
+function x_out = map(x, in_min, in_max, out_min, out_max)
+    x_out = (x - in_min) .* (out_max - out_min) ./ (in_max - in_min) + out_min;
+end
+
+% This function takes the non-zero low end of a histogram and bump it to an
+% upper level
+function Iout = bump_hist_channel(I, bgI, upper)
+    thres_lo = 10;
+    thres_hi = 300;
+    h = imhist(bgI);
+    h = h(2:end);
+    for k = 1:size(h, 1)
+        if h(k) > thres_hi
+            break
+        end
+    end
+    f = (upper - k) / 255 + 1;
+    Iout = I .* f;
+end
+
+function Iout = bumpEqualize(I, bgI, upper)
+    % For each RGB channels
+    Iout = I;
+    Iout(:, :, 1) = bump_hist_channel(I(:, :, 1), bgI(:, :, 1), upper);
+    Iout(:, :, 2) = bump_hist_channel(I(:, :, 2), bgI(:, :, 2), upper);
+    Iout(:, :, 3) = bump_hist_channel(I(:, :, 3), bgI(:, :, 3), upper);
 end
