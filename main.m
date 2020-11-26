8f614c0af0dc20ff348e11b7f8143872ddbfac87
Muchen He <i@muchen.ca>
2020-11-25 16:08:31 -0800

    clean up

--- main.m
+++ main.m
@@ -33,128 +33,16 @@ img_grad = edge_grad(img_bin, thres);
 
 % Make bright spots more visible
 H = H .^ 2;
-imshow(H, []);
 
-%% Find points from H (1.75 seconds)
-% ratio = 0.7;
-% while ratio > 0.1
-%     ratio = ratio - 0.01;
-    
-%     mask = H > ratio*max(H(:));
-%     mask = bwmorph(mask, 'dilate', 15);
+%% Find points from H (binary search) (0.7 seconds vs 1.75s of old method)
+peaks = hough_search(H, 4, 10);
+lines = houghlines(img_bw, T, R, peaks);
 
-%     if (SHOW_DEBUG)
-%         imshow(mask, []);
-%     end
-%     mask = bwmorph(mask, 'shrink', Inf);
-%     mask = double(mask);
+line_verts = hough_lines2verts(lines);
+points = intersection_points(line_verts, img);
+draw_detection(img, line_verts, points);
 
-%     [r_idx, t_idx] = find(mask);
-%     peaks = [r_idx, t_idx];
-    
-%     if size(peaks, 1) >= 4
-%         break
-%     end
-% end
-
-%% Find points from H (binary search) (0.7 seconds)
-ratio_hi = 1;
-ratio_lo = 0;
-epoch = 0;
-while epoch < 10
-    ratio = (ratio_hi - ratio_lo) / 2 + ratio_lo;
-
-    mask = H > ratio * max(H(:));
-    mask = bwmorph(mask, 'dilate', 15);
-    mask = bwmorph(mask, 'shrink', Inf);
-    mask = double(mask);
-
-    [r_idx, t_idx] = find(mask);
-    peaks = [r_idx, t_idx];
-
-    if size(peaks, 1) > 4
-        % if number of peaks > 4 then we need to decrease high
-        ratio_hi = ratio;
-    elseif size(peaks, 1) < 4
-        ratio_lo = ratio;
-    else
-        break
-    end
-    
-    epoch = epoch + 1;
-end
-
-return;
-
-%% Find peak points furtherest apart
-% opts = nchoosek(1:size(peaks, 1), 4);
-% max_area = 0;
-% max_peaks = peaks;
-% for i = 1:size(opts, 1)
-%     P = zeros(4, 2);
-%     for j = 1:4
-%         P(j, :) = peaks(opts(i, j), :);
-%     end
-%     [hull, area] = convhull(P);
-    
-%     if area > max_area
-%         max_peaks = P;
-%     end
-% end
-% peaks = max_peaks;
-
-%% Draw line on image
-figure;
-hold on;
-imshow(img_bw, []);
-lines = houghlines(img_bw,T,R, peaks);
-% for i = 1:size(r_idx, 1)
-%     r = R(r_idx(i));
-%     t = T(t_idx(i));
-%     [x, y] = pol2cart(t, r);
-%     line([0, x], [0, y]);
-% end
-
-line_verts = zeros(4, 2, 2);
-
-hold on
-for k = 1:numel(lines)
-    x1 = lines(k).point1(1);
-    y1 = lines(k).point1(2);
-    x2 = lines(k).point2(1);
-    y2 = lines(k).point2(2);
-    plot([x1 x2],[y1 y2],'Color','g','LineWidth', 2)
-    
-    line_verts(k,1,:) = [x1 x2];
-    line_verts(k,2,:) = [y1 y2];
-end
-
-% Find intersection
-points = [];
-index_opts = nchoosek(1:4, 2);
-for i = 1:size(index_opts, 1)
-    l1_idx = index_opts(i, 1);
-    l2_idx = index_opts(i, 2);
-    
-    x1 = line_verts(l1_idx,1,:);
-    y1 = line_verts(l1_idx,2,:);
-    x2 = line_verts(l2_idx,1,:);
-    y2 = line_verts(l2_idx,2,:);
-    
-    [x_i, y_i] = intersection(x1, y1, x2, y2);
-    
-    % discard points outside of image
-    if x_i < 0 || x_i > size(img, 2) || y_i < 0 || y_i > size(img, 1)
-        continue
-    end
-    points = [points; x_i y_i];
-    plot(x_i, y_i, 'r*')
-end
-hold off
-
-
-%% 
-
+%%
 % Control (original image)
 h = size(img, 1);
 w = 4/3 * h;
@@ -242,10 +130,96 @@ function img = edge_grad(I, thres)
     img(img < thres) = 0;
 end
 
+function points = hough_search(H, n, max_epoch)
+    ratio_hi = 1.0;
+    ratio_lo = 0.0;
+    epoch = 0;
+
+    while epoch < max_epoch
+        ratio = (ratio_hi - ratio_lo) / 2 + ratio_lo;
+
+        mask = H > ratio * max(H(:));
+        mask = bwmorph(mask, 'dilate', 15);
+        mask = bwmorph(mask, 'shrink', Inf);
+        mask = double(mask);
+
+        [r_idx, t_idx] = find(mask);
+        points = [r_idx, t_idx];
+
+        if size(points, 1) > n
+            ratio_hi = ratio;
+        elseif size(points, 1) < n
+            ratio_lo = ratio;
+        else
+            break
+        end
+        
+        epoch = epoch + 1;
+    end
+end
+
+% This function finds the lines in spatial/px coordinates from hough lines
+function line_verts = hough_lines2verts(lines)
+    line_verts = zeros(4, 2, 2);
+
+    for k = 1:numel(lines)
+        x1 = lines(k).point1(1);
+        y1 = lines(k).point1(2);
+        x2 = lines(k).point2(1);
+        y2 = lines(k).point2(2);
+        
+        line_verts(k,1,:) = [x1 x2];
+        line_verts(k,2,:) = [y1 y2];
+    end
+end
+
+function points = intersection_points(line_verts, img)
+    points = [];
+    index_opts = nchoosek(1:4, 2);
+    for i = 1:size(index_opts, 1)
+        l1_idx = index_opts(i, 1);
+        l2_idx = index_opts(i, 2);
+        
+        x1 = line_verts(l1_idx,1,:);
+        y1 = line_verts(l1_idx,2,:);
+        x2 = line_verts(l2_idx,1,:);
+        y2 = line_verts(l2_idx,2,:);
+        
+        [x_i, y_i] = intersection(x1, y1, x2, y2);
+        
+        % discard points outside of image
+        if x_i < 0 || x_i > size(img, 2) || y_i < 0 || y_i > size(img, 1)
+            continue
+        end
+
+        points = [points; x_i y_i];
+    end
+end
+
 % https://www.mathworks.com/matlabcentral/answers/70287-to-find-intersection-point-of-two-lines
 function [x, y] = intersection(x1, y1, x2, y2)
-p1 = polyfit(x1, y1, 1);
-p2 = polyfit(x2, y2, 1);
-x = fzero(@(x) polyval(p1 - p2, x), 3);
-y = polyval(p1, x);
+    p1 = polyfit(x1, y1, 1);
+    p2 = polyfit(x2, y2, 1);
+    x = fzero(@(x) polyval(p1 - p2, x), 3);
+    y = polyval(p1, x);
+end
+
+% Draw picture with detected lines and intersection points
+function draw_detection(img, line_verts, points)
+    figure;
+    imshow(img);
+
+    hold on
+    for k = 1:size(line_verts, 1)
+        x1 = line_verts(k, 1, 1);
+        x2 = line_verts(k, 1, 2);
+        y1 = line_verts(k, 2, 1);
+        y2 = line_verts(k, 2, 2);
+        plot([x1 x2],[y1 y2],'Color','g','LineWidth', 2);
+    end
+
+    for k = 1:size(points, 1)
+        plot(points(k, 1), points(k, 2), 'r*');
+    end
+    hold off
 end
