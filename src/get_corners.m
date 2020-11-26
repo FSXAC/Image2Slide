e41a9ed335adb10cd03c4247731b8fd12aae637f
Muchen He <i@muchen.ca>
2020-11-25 19:46:34 -0800

    added get corners

---
+++ src/get_corners.m
@@ -0,0 +1,39 @@
+% 1. Downsample the image
+% 2. Do regular edge detection
+% 3. resovle the corners to be in normalized coordinates
+% 4. return the normalized coordinates in form (0-0, 0-w, h-0, h-w) form
+function norm_points = get_corners(I_bw)
+    I_edge = edge(I_bw);
+
+    HOUGH_RHO_RES = 1 + floor(max(size(I_bw)) / 1000);
+    HOUGH_THETA_RES = 0.2;
+    
+    [H, T, R] = hough(I_edge, 'RhoResolution', HOUGH_RHO_RES ,'Theta', -90:HOUGH_THETA_RES:89);
+    H = H .^ 2;
+
+    peaks = hough_search(H, 4, 50, 5);
+    lines = houghlines(I_bw, T, R, peaks);
+
+    line_verts = hough_lines2verts(lines);
+    points = intersection_points(line_verts, I_bw);
+    draw_detection(I_bw, line_verts, points);
+
+    npoints = zeros(size(points));
+    npoints(:, 1) = points(:, 1) ./ size(I_bw, 2); % divide by width
+    npoints(:, 2) = points(:, 2) ./ size(I_bw, 1); % divide by height
+
+    % Sort the coordinates from top left to bottom right
+    tp = norm_points - 0.5;
+    
+    t = zeros([4, 2]);
+    for j = 1:size(tp, 1)
+        x = tp(j, 1);
+        y = tp(j, 2);
+        t(j, :) = [atan2(y, x), j];
+    end
+    idxs = sortrows(t);
+    norm_points = npoints;
+    for j = 1:size(npoints,1)
+        norm_points(j,:) = npoints(idxs(j, 2), :);
+    end
+end
--- src/main.m
+++ src/main.m
@@ -5,39 +5,18 @@ clear;
 close all;
 
 % Parameters
-in_file = 'ieee_xs.jpg';
+in_file = 'img/ieee_xs.jpg';
 out_file = 'classroom_scan.png';
 
 img = imread(in_file);
 
 SHOW_DEBUG = false;
-HOUGH_RHO_RES = 1 + floor(max(size(img)) / 1000);
-HOUGH_THETA_RES = 0.2;
 
 % Convert to greyscale
 img_bw = uint8(rgb2gray(img));
 [img_bin, thres] = binaryImage2(img_bw);
 
-% Apply edge detection filter (note: sobel is not suitable because lines could be diagonal)
-img_grad = edge_grad(img_bin, thres);
-img_edge = edge(img_bw);
-
-% Hough transform
-[H, T, R] = hough(img_edge, 'RhoResolution', HOUGH_RHO_RES ,'Theta', -90:HOUGH_THETA_RES:89);
-
-% Make bright spots more visible
-H = H .^ 2;
-
-% imshow(imgradient(H), []);
-% return;
-
-% Find points from H (binary search) (0.7 seconds vs 1.75s of old method)
-peaks = hough_search(H, 4, 50, 5);
-lines = houghlines(img_bw, T, R, peaks);
-
-line_verts = hough_lines2verts(lines);
-points = intersection_points(line_verts, img);
-draw_detection(img, line_verts, points);
+npoints = get_corners(img_bw);
 
 %% rectify image
 [img_rectified, transformation, ref] = rectify_image(img, points, 16/9);
